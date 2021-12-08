a56424c43f3e485fa1ae05fd29105e8324b139ce
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-12-08 01:21:10 -0300

    Criação de likes e letra maiúscula no objeto

--- modelo.py
+++ modelo.py
@@ -1,19 +1,30 @@
 class Filme:
     def __init__(self,nome,ano,duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
 
+    def dar_like(self):
+        self.likes += 1
 
 class Serie:
     def __init__(self,nome,ano,temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+            self.likes += 1
 
 
-vingadores = Filme('vingadores', 2018,160)
-print(vingadores.nome)
+filme = Filme('vingadores - guerra infinita', 2018,160)
+filme.dar_like()
 
-bad_batch = Serie('Bad Batch', 2021, 1)
-print("A serie é {} lançada no ano {} e que tem {} temporada(s) ".format(bad_batch.nome,bad_batch.ano,bad_batch.temporadas))
+print("O filme se chama {} foi lançado em {} e tem a duração de {} minutos. Teve {} likes".format(filme.nome,filme.ano,filme.duracao,filme.likes))
+
+serie = Serie('Bad batch', 2021, 1)
+serie.dar_like()
+serie.dar_like()
+print("A serie é {} lançada no ano {} e que tem {} temporada(s).Teve {} likes".format(serie.nome,serie.ano,serie.temporadas,serie.likes))
