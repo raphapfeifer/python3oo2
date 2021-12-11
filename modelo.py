b565797acfd770c7bc1a6bcaf3292e4f811de753
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-12-11 00:23:56 -0300

    Criação de property

--- modelo.py
+++ modelo.py
@@ -1,22 +1,46 @@
 class Filme:
     def __init__(self,nome,ano,duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self,nome):
+        self.__nome = nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
 
 class Serie:
     def __init__(self,nome,ano,temporadas):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_like(self):
-            self.likes += 1
+            self.__likes += 1
 
 
 filme = Filme('vingadores - guerra infinita', 2018,160)
