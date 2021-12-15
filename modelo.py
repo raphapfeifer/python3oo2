e44ff36255ed938ae029d1206668dfbea5cc284b
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-12-14 23:59:38 -0300

    Criação de classe mãe

--- modelo.py
+++ modelo.py
@@ -1,46 +1,42 @@
-class Filme:
-    def __init__(self,nome,ano,duracao):
-        self.__nome = nome.title()
+class Programa:
+    def __init__(self,nome,ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self,nome):
-        self.__nome = nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-class Serie:
-    def __init__(self,nome,ano,temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
-        self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, nome):
-        self.__nome = nome.title()
+        self._nome = nome.title()
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
     def dar_like(self):
-            self.__likes += 1
+        self._likes += 1
+
+
+class Filme(Programa):
+    def __init__(self,nome,ano,duracao):
+        self._nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self._likes = 0
+
+
+
+class Serie(Programa):
+    def __init__(self,nome,ano,temporadas):
+        self._nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self._likes = 0
+
+
 
 
 filme = Filme('vingadores - guerra infinita', 2018,160)
