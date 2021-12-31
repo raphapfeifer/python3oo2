b05f94340a8f2acf08daf6edf07330277d96c249
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2021-12-30 23:43:44 -0300

    criação de método str

--- modelo.py
+++ modelo.py
@@ -19,24 +19,24 @@ class Programa:
     def dar_like(self):
         self._likes += 1
 
-    def imprime(self):
-        print({self._nome} - {self.ano} - {self._likes})
+    def __str__(self):
+        return self._nome + " " + str(self.ano) + " " + str(self._likes)
 
 class Filme(Programa):
     def __init__(self,nome,ano,duracao):
         super().__init__(nome,ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print({self._nome} - {self.ano} - {self.duracao} - {self._likes})
+    def __str__(self):
+        return  self._nome + " " + str(self.ano) + " " + str(self.duracao) + " " + str(self._likes)
 
 class Serie(Programa):
     def __init__(self,nome,ano,temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print({self._nome} - {self.ano} - {self.temporadas} - {self._likes})
+    def __str__(self):
+        return self._nome + " " + str(self.ano) + " " + str(self.temporadas) + " " + str(self._likes)
 
 
 filme = Filme('vingadores - guerra infinita', 2018,160)
@@ -49,4 +49,4 @@ serie.dar_like()
 filmes_e_series = [filme,serie]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
